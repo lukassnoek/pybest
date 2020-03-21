98f5b077939eff6b47093b020f6363b6885bf7d4
Lukas Snoek <lukassnoek@gmail.com>
2020-03-21 14:41:42 +0100

    Add code to gather data

--- pybest/cli.py
+++ pybest/cli.py
@@ -1,3 +1,4 @@
+import os
 import click
 import os.path as op
 from glob import glob
@@ -7,19 +8,31 @@ from .signal_model import optimize_signal_model
 
 
 @click.command()
-@click.argument('fprep_dir')
+@click.argument('bids_dir')
 @click.argument('out_dir', default=None, required=False)
+@click.argument('fprep_dir', default=None, required=False)
 @click.option('--participant-label', default=None, required=False)
 @click.option('--session', default=None, required=False)
 @click.option('--task', default=None)
 @click.option('--space', default='T1w')
-def main(fprep_dir, out_dir, participant_label, session, task, space):
+@click.option('--hemi', default='L')
+def main(bids_dir, out_dir, fprep_dir, participant_label, session, task, space, hemi):
     """ Main API of pybest. """
 
-    logger.info('Starting pybest!')
-
+    logger.info(f"Working on BIDS directory {bids_dir}")
     if out_dir is None:  # Set default out_dir
-        out_dir = op.join(op.dirname(fprep_dir), 'pybest')
+        out_dir = op.join(bids_dir, 'derivatives', 'pybest')
+        if not op.isdir(out_dir):
+            os.makedirs(out_dir)
+
+        logger.info(f"Setting output directory to {out_dir}")
+
+    if fprep_dir is None:
+        fprep_dir = op.join(bids_dir, 'derivatives', 'fmriprep')
+        if not op.isdir(fprep_dir):
+            raise ValueError(f"Fmriprep directory {fprep_dir} does not exist.")
+
+        logger.info(f"Setting Fmriprep directory to {fprep_dir}")
 
     # Use all possible participants if not provided
     if participant_label is None:
@@ -45,32 +58,55 @@ def main(fprep_dir, out_dir, participant_label, session, task, space):
             session.append(this_sess)
             logger.info(f"Found {len(this_sess)} session(s) for sub-{participant}")
     else:
-        session = [session]
+        session = [session] * len(participant_label)
 
     # Use all tasks if not provided
     if task is None:
+        task = []
         for participant, this_ses in zip(participant_label, session):
+            sub_tasks = []
             for ses in this_ses:
                 tmp = glob(op.join(
-                    fprep_dir,
+                    bids_dir,
                     f'sub-{participant}',
                     f'ses-{ses}',
                     'func',
                     f'*_events.tsv'
                 ))
 
-                task = list(set(
+                these_task = list(set(
                     [op.basename(f).split('task-')[1].split('_')[0] for f in tmp]
                 ))
-                logger.info(f"Found {len(task)} tasks for sub-{participant} and ses-{ses}")
+                sub_tasks.append(these_task)
+                logger.info(f"Found {len(these_task)} task(s) for sub-{participant} and ses-{ses}")
 
-    # Find files, construct Dataset obj
+            task.append(sub_tasks)
+    else:
+        task = [[task] * len(session)] * len(participant_label)
 
-    # Preprocess
+    space_idf = f'hemi-{hemi}.func.gii' if 'fs' in space else 'desc-preproc_bold.nii.gz'
 
-    # Noise model
+    for i, participant in enumerate(participant_label):
+        for ii, ses in enumerate(session[i]):
+            for iii, task in enumerate(task[i][ii]):
+                logger.info(f"Starting process for sub-{participant}, ses-{ses}, task-{task}")
 
-    # Signal model
+                # Gather funcs, confs, tasks
+                funcs = sorted(glob(op.join(
+                    fprep_dir, f'sub-{participant}', f'ses-{ses}', 'func', f'*task-{task}_*_space-{space}_{space_idf}'
+                )))
+                confs = sorted(glob(op.join(
+                    fprep_dir, f'sub-{participant}', f'ses-{ses}', 'func', f'*desc-confounds_regressors.tsv'
+                )))
+                events = sorted(glob(op.join(
+                    bids_dir, f'sub-{participant}', f'ses-{ses}', 'func', f'*task-{task}_*_events.tsv'
+                )))
+                if not all(len(funcs) == len(tmp) for tmp in [confs, events]):
+                    raise ValueError(
+                        f"Found unequal number of funcs ({len(funcs)}), confs ({len(confs)}), and events ({len(events)})."
+                    )
+                logger.info(f"Found {len(funcs)} runs of task {task}")
+
 
 if __name__ == '__main__':
 
