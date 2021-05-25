dfc4a5bfbbc00a21223126ea106fa93cf453f5a6
Ella <ella.casimiro@live.be>
2021-05-25 15:08:58 +0200

    remove transpose in vstack

--- pybest/utils.py
+++ pybest/utils.py
@@ -140,7 +140,7 @@ def load_and_split_cifti(cifti, indices_file, left_id=None, right_id=None, subc_
 
 
     else:
-        data = np.vstack([l.T, r.T])
+        data = np.vstack([l, r])
         return data.T, s
 
 
